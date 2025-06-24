3f4f53a06f81a72bf851e154b7365ee70c637b97
tamaranovic <novic.tamara@gmail.com>
2025-06-24 11:20:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    var navLinksContainer = document.getElementById("nav-links-container");
+    navLinksContainer.classList.toggle("open");
+});
